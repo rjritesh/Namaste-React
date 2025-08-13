c3d2a0736e1fb77e2f789b191383e322a68d8751
Ritesh <riteshjha24060@gmail.com>
2025-08-13 22:18:47 +0530

    Initial setup

---
+++ NetflixGPT/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold text-amber-500'>
+      Welcome, to NetflixGPT !!
+
+    </div>
+  )
+}
+
+
+export default App
