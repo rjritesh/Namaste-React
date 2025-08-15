c183b301ec94b635dbb13ac3313a97416a4b4493
Ritesh <riteshjha24060@gmail.com>
2025-08-15 18:22:14 +0530

    Form Validation

--- NetflixGPT/src/components/Login.jsx
+++ NetflixGPT/src/components/Login.jsx
@@ -1,11 +1,19 @@
-
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Header from './Header'
+import { FormValidate } from "../utils/Validate"
+
 
 const Login = () => {
-  const [isSignIn, setisSignIn] = useState(true)
+  const [isSignIn, setisSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null)
 
+  const email = useRef(null);
+  const password = useRef(null);
 
+  const handleBtnClick = () => {
+    const message = FormValidate(email.current.value, password.current.value);
+    setErrorMessage(message)
+  }
 
   return (
     <div>
@@ -20,21 +28,24 @@ const Login = () => {
       </div>
 
       {/* Login form */}
-      <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 opacity-90 flex flex-col items-center rounded-sm">
+      <form onSubmit={(e) => e.preventDefault()} className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 opacity-90 flex flex-col items-center rounded-sm">
         <h1 className='text-white font-bold text-4xl mb-8 items-end self-start'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
         {!isSignIn && (<input type='text' required placeholder='Name' className="p-2 m-2 w-full placeholder-gray-300 border rounded-sm border-gray-300  text-white bg-zinc-900"></input>)}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address" required
           className="p-2 m-2 w-full placeholder-gray-300 border rounded-sm border-gray-300  text-white bg-zinc-900"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password" required
           className="p-2 m-2 w-full placeholder-gray-300 border rounded-sm border-gray-300  text-white bg-zinc-900"
         />
+        <p className='text-red-500 self-start font-semibold'>{errorMessage}</p>
 
-        <button className="p-3 m-4 bg-red-600 font-bold rounded-sm text-white w-full cursor-pointer hover:bg-red-700">{isSignIn ? "Sign In" : "Sign Up"}</button>
+        <button className="p-3 m-4 bg-red-600 font-bold rounded-sm text-white w-full cursor-pointer hover:bg-red-700" onClick={handleBtnClick} >{isSignIn ? "Sign In" : "Sign Up"}</button>
         <span className='text-gray-100'>OR</span>
 
         <button className="p-3 m-4 bg-gray-600 font-bold rounded-sm text-white w-full cursor-pointer">Use a Sign-In code</button>
