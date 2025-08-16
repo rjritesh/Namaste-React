ed237620f99b7ca06b564e6b633985b5a5b2aef6
Ritesh <riteshjha24060@gmail.com>
2025-08-16 17:15:42 +0530

    Firebase setup

---
+++ NetflixGPT/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaszBcORKD9Jo0tNScocAuECQ5Ty5Lh88",
+  authDomain: "netflixgpt-bb901.firebaseapp.com",
+  projectId: "netflixgpt-bb901",
+  storageBucket: "netflixgpt-bb901.firebasestorage.app",
+  messagingSenderId: "288848763271",
+  appId: "1:288848763271:web:59487392068259b8b5db22"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
