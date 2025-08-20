6f6b191c8f98b4de08a6fa587c18644be065610d
Ritesh <riteshjha24060@gmail.com>
2025-08-20 22:32:50 +0530

    Update

--- NetflixGPT/src/App.jsx
+++ NetflixGPT/src/App.jsx
@@ -1,13 +1,21 @@
 
-import Body from './components/Body';
+import Login from "./components/Login"
+import Browse from "./components/Browse"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
 
 const App = () => {
   return (
     <div className='text-white'>
-     
-        <Body />
+      <Body></Body>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login></Login>} />
+          <Route path="/browse" element={<Browse />} />
+        </Routes>
+      </BrowserRouter>
 
-      
+
     </div>
   );
 }
--- NetflixGPT/src/components/Body.jsx
+++ NetflixGPT/src/components/Body.jsx
@@ -1,7 +1,4 @@
 
-import Login from "../components/Login"
-import Browse from "../components/Browse"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Body = () => {
 
@@ -9,16 +6,8 @@ const Body = () => {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Login></Login>} />
-
-
-        <Route path="/browse" element={<Browse />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <></>
+  )
 };
 
 export default Body;
