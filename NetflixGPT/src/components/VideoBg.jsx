a73fa9688b3347bb1165b701a459bd3f0323d77a
Ritesh <riteshjha24060@gmail.com>
2025-08-29 17:41:41 +0530

    Fixed layout

--- NetflixGPT/src/components/VideoBg.jsx
+++ NetflixGPT/src/components/VideoBg.jsx
@@ -8,7 +8,7 @@ const VideoBg = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div><iframe className="w-screen aspect-video" src={"https://www.youtube.com/embed/" +
+    <div><iframe className="w-full aspect-video" src={"https://www.youtube.com/embed/" +
       trailerVideo?.key +
       "?autoplay=1&mute=1&controls=0&showinfo=0&rel=0"} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe></div>
   )
